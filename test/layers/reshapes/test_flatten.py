eb7d95778df9678f23db2407cb777ac8e55904fa
bourcierj <jules.bourcier@earthcube.eu>
2022-07-03 01:21:59 +0200

    Add test for flatten

---
+++ test/layers/reshapes/test_flatten.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch.nn as nn
+import pytest
+
+from test.utils import convert_and_test
+
+
+class LayerTest(nn.Module):
+    def __init__(self):
+        super(LayerTest, self).__init__()
+
+    def forward(self, x):
+        x = x.flatten(1)
+        return x
+
+
+@pytest.mark.parametrize('change_ordering', [True, False])
+def test_flatten(change_ordering):
+    model = LayerTest()
+    model.eval()
+
+    input_np = np.random.uniform(0, 1, (1, 1, 512))
+    error = convert_and_test(model, input_np, verbose=False, change_ordering=change_ordering)
