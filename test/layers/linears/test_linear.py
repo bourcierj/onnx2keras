d32bdb506252152ccb23fcad057f43b9bbe624ae
bourcierj <jules.bourcier@earthcube.eu>
2022-07-12 22:21:30 +0200

    Make `test_linear` test deterministic wrt its parameters

--- test/layers/linears/test_linear.py
+++ test/layers/linears/test_linear.py
@@ -8,9 +8,9 @@ from test.utils import convert_and_test
 
 
 class LayerTest(nn.Module):
-    def __init__(self, inp, out, bias=False):
+    def __init__(self, in_features, out_features, bias=False):
         super(LayerTest, self).__init__()
-        self.fc = nn.Linear(inp, out, bias=bias)
+        self.fc = nn.Linear(in_features, out_features, bias=bias)
 
     def forward(self, x):
         x = self.fc(x)
@@ -19,11 +19,12 @@ class LayerTest(nn.Module):
 
 @pytest.mark.parametrize('change_ordering', [True, False])
 @pytest.mark.parametrize('bias', [True, False])
-def test_linear(change_ordering, bias):
-    ins = np.random.choice([1, 3, 7, 128])
+@pytest.mark.parametrize('in_features', [1, 128])
+@pytest.mark.parametrize('out_features', [1, 128])
+def test_linear(change_ordering, bias, in_features, out_features):
 
-    model = LayerTest(ins, np.random.choice([1, 3, 7, 128]), bias=bias)
+    model = LayerTest(in_features, out_features, bias=bias)
     model.eval()
 
-    input_np = np.random.uniform(0, 1, (1, ins))
+    input_np = np.random.uniform(0, 1, (1, in_features))
     error = convert_and_test(model, input_np, verbose=False, change_ordering=change_ordering)
