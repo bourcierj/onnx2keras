30975f1ab8030f81c7add49978faa2f2ed757480
bourcierj <jules.bourcier@earthcube.eu>
2022-07-12 22:26:28 +0200

    Expect possible RuntimeError exception in `test_avgpool*` and `test_maxpool*` tests.
    Instead of skipping the tests; this is better practice to catch a possible exception.

--- test/layers/poolings/test_avgpool3d.py
+++ test/layers/poolings/test_avgpool3d.py
@@ -1,3 +1,5 @@
+import contextlib
+
 import numpy as np
 import torch.nn as nn
 import pytest
@@ -24,12 +26,15 @@ class LayerTest(nn.Module):
 def test_avgpool3d(change_ordering, kernel_size, padding, stride):
     if not tf.test.gpu_device_name() and not change_ordering:
         pytest.skip("Skip! Since tensorflow AvgPoolingOp op currently only supports the NHWC tensor format on the CPU")
-    if padding > kernel_size / 2:
-        # RuntimeError: invalid argument 2: pad should be smaller than half of kernel size,
-        # but got padW = 1, padH = 1, kW = 1,
-        pytest.skip("pad should be smaller than half of kernel size")
+
     model = LayerTest(kernel_size=kernel_size, padding=padding, stride=stride)
     model.eval()
 
     input_np = np.random.uniform(0, 1, (1, 3, 20, 224, 224))
-    error = convert_and_test(model, input_np, verbose=False, change_ordering=change_ordering)
+
+    if padding > kernel_size / 2:
+        cm = pytest.raises(RuntimeError, match="pad should be smaller than or equal to half of kernel size")
+    else:
+        cm = contextlib.nullcontext()
+    with cm:
+        error = convert_and_test(model, input_np, verbose=False, change_ordering=change_ordering)
