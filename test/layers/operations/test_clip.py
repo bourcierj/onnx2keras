96d6b6f2d6f504374f8d6b86ef3b56f8d54b1247
bourcierj <jules.bourcier@earthcube.eu>
2022-07-17 19:34:34 +0200

    Add test with `clip` for `raise_error_on_lambda_layers=True`

--- test/layers/operations/test_clip.py
+++ test/layers/operations/test_clip.py
@@ -2,7 +2,8 @@ import torch.nn as nn
 import numpy as np
 import pytest
 
-from test.utils import convert_and_test
+from test.utils import convert_and_test, convert
+from onnx2keras.exceptions import LambdaLayerError
 
 
 class FClipTest(nn.Module):
@@ -26,3 +27,13 @@ def test_clip(change_ordering):
     input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
 
     error = convert_and_test(model, input_np, verbose=False, change_ordering=change_ordering)
+
+
+@pytest.mark.parametrize('change_ordering', [True, False])
+def test_clip_raise_error_on_lambda_layers(change_ordering):
+    model = FClipTest()
+    model.eval()
+
+    input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+    with pytest.raises(LambdaLayerError) as excinfo:
+        k_model = convert(model, input_np, verbose=False, change_ordering=change_ordering, raise_error_on_lambda_layers=True)
--- test/utils.py
+++ test/utils.py
@@ -6,7 +6,13 @@ import onnx
 from onnx2keras import onnx_to_keras, check_torch_keras_error
 
 
-def torch2keras(model: torch.nn.Module, input_variable, keras_input_shapes=None, name_policy=None, verbose=True, change_ordering=False):
+def torch2keras(model: torch.nn.Module,
+                input_variable,
+                keras_input_shapes=None,
+                name_policy=None,
+                verbose=True,
+                change_ordering=False,
+                raise_error_on_lambda_layers=False):
     if isinstance(input_variable, (tuple, list)):
         input_variable = tuple(torch.FloatTensor(var) for var in input_variable)
         input_names = [f'test_in{i}' for i, _ in enumerate(input_variable)]
@@ -19,7 +25,7 @@ def torch2keras(model: torch.nn.Module, input_variable, keras_input_shapes=None,
                       output_names=['test_out'])
     temp_f.seek(0)
     onnx_model = onnx.load(temp_f)
-    k_model = onnx_to_keras(onnx_model, input_names, input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering)
+    k_model = onnx_to_keras(onnx_model, input_names, input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering, raise_error_on_lambda_layers=raise_error_on_lambda_layers)
     return k_model
 
 
@@ -28,9 +34,10 @@ def convert(model: torch.nn.Module,
             keras_input_shapes=None,
             name_policy=None,
             verbose=True,
-            change_ordering=False):
+            change_ordering=False,
+            raise_error_on_lambda_layers=False):
     #@todo: add type hints and docstring
-    return torch2keras(model, input_variable, keras_input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering)
+    return torch2keras(model, input_variable, keras_input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering, raise_error_on_lambda_layers=raise_error_on_lambda_layers)
 
 
 def convert_and_test(model: torch.nn.Module,
@@ -39,8 +46,9 @@ def convert_and_test(model: torch.nn.Module,
                      name_policy=None,
                      verbose=True,
                      change_ordering=False,
+                     raise_error_on_lambda_layers=False,
                      epsilon=1e-5):
-    k_model = torch2keras(model, input_variable, keras_input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering)
+    k_model = torch2keras(model, input_variable, keras_input_shapes=keras_input_shapes, name_policy=name_policy, verbose=verbose, change_ordering=change_ordering, raise_error_on_lambda_layers=raise_error_on_lambda_layers)
 
     error = check_torch_keras_error(model, k_model, input_variable, change_ordering=change_ordering, epsilon=epsilon)
     return error
